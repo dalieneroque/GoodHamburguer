24469fa7a3b5f98ca75e0d5071bafaf76b3435a8
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-20 19:26:05 -0300

    feat(Domain): adiciona entidade Pedido e enums de produtos

---
+++ GoodHamburguer.Domain/Entities/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using GoodHamburguer.Domain.Enums;
+
+namespace GoodHamburguer.Domain.Entidades;
+
+public class Pedido
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public Sanduiche? Sanduiche { get; set; }
+    public Acompanhamento? Acompanhamento { get; set; }
+    public Bebida? Bebida { get; set; }
+
+    public decimal Subtotal { get; set; }
+    public decimal Desconto { get; set; }
+    public decimal Total { get; set; }
+}
---
+++ GoodHamburguer.Domain/Enums/Enums.cs
@@ -0,0 +1,18 @@
+﻿namespace GoodHamburguer.Domain.Enums;
+
+public enum Sanduiche
+{
+    XBurger,
+    XEgg,
+    XBacon
+}
+
+public enum Acompanhamento
+{
+    BatataFrita
+}
+
+public enum Bebida
+{
+    Refrigerante
+}
