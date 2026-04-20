11426d1d09d9a79be350f8ba9222a890091ef651
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-20 19:57:31 -0300

    feat(Infrastructure): adicionar AppDbContext para acesso a dados

---
+++ GoodHamburguer.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GoodHamburguer.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodHamburguer.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Pedido> Pedidos { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+}
