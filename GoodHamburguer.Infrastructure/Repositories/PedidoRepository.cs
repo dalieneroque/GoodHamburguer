92d76afd46b34848b5e0abeac2a625c49e323e2d
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-20 20:02:59 -0300

    feat(Infraestrutura): implementar repositório de pedidos

---
+++ GoodHamburguer.Infrastructure/Repositories/PedidoRepository.cs
@@ -0,0 +1,43 @@
+﻿using GoodHamburguer.Domain.Entidades;
+using GoodHamburguer.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodHamburguer.Infrastructure.Repositories;
+
+public class PedidoRepository
+{
+    private readonly AppDbContext _context;
+
+    public PedidoRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Adicionar(Pedido pedido)
+    {
+        await _context.Pedidos.AddAsync(pedido);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<Pedido>> ObterTodos()
+    {
+        return await _context.Pedidos.ToListAsync();
+    }
+
+    public async Task<Pedido?> ObterPorId(Guid id)
+    {
+        return await _context.Pedidos.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task Remover(Pedido pedido)
+    {
+        _context.Pedidos.Remove(pedido);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Atualizar(Pedido pedido)
+    {
+        _context.Pedidos.Update(pedido);
+        await _context.SaveChangesAsync();
+    }
+}
