4f5f79fc1ced32eaf6df9d719c4e61a3cbef5038
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-20 22:08:30 -0300

    feat(Application): adiciona DTO PedidoResponse

---
+++ GoodHamburguer.Application/DTOs/PedidoResponse.cs
@@ -0,0 +1,13 @@
+﻿using GoodHamburguer.Domain.Enums;
+
+namespace GoodHamburguer.API.DTOs;
+
+public record PedidoResponse(
+    Guid Id,
+    Sanduiche? Sanduiche,
+    Acompanhamento? Acompanhamento,
+    Bebida? Bebida,
+    decimal Subtotal,
+    decimal Desconto,
+    decimal Total
+);
