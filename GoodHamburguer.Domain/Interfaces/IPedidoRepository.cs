ee786d2b81894b5fbf718284218acb02292a80ad
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-20 21:28:48 -0300

    feat(dominio): adiciona interface IPedidoRepository

---
+++ GoodHamburguer.Domain/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,12 @@
+﻿using GoodHamburguer.Domain.Entidades;
+
+namespace GoodHamburguer.Domain.Interfaces;
+
+public interface IPedidoRepository
+{
+    Task AdicionarAsync(Pedido pedido);
+    Task<List<Pedido>> ObterTodosAsync();
+    Task<Pedido?> ObterPorIdAsync(Guid id);
+    Task AtualizarAsync(Pedido pedido);
+    Task RemoverAsync(Pedido pedido);
+}
