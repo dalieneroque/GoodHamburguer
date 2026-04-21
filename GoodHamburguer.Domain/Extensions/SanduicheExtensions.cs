149a8371dff25911d63281e5c13f18a677983cda
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-20 21:20:13 -0300

    feat(dominio): adiciona extension method ObterPreco em Sanduiche

---
+++ GoodHamburguer.Domain/Extensions/SanduicheExtensions.cs
@@ -0,0 +1,14 @@
+﻿using GoodHamburguer.Domain.Enums;
+
+namespace GoodHamburguer.Domain.Extensions;
+
+public static class SanduicheExtensions
+{
+    public static decimal ObterPreco(this Sanduiche sanduiche) => sanduiche switch
+    {
+        Sanduiche.XBurger => 5.00m,
+        Sanduiche.XEgg => 4.50m,
+        Sanduiche.XBacon => 7.00m,
+        _ => throw new ArgumentOutOfRangeException(nameof(sanduiche))
+    };
+}
