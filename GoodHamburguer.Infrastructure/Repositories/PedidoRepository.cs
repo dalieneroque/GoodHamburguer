2552e4e2fd4941f7b0810b2d0f7c04136c2c7ae4
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-20 21:33:56 -0300

    refactor(Infrastructure): PedidoRepository implementa IPedidoRepository

--- GoodHamburguer.Infrastructure/Repositories/PedidoRepository.cs
+++ GoodHamburguer.Infrastructure/Repositories/PedidoRepository.cs
@@ -1,10 +1,11 @@
 ﻿using GoodHamburguer.Domain.Entidades;
+using GoodHamburguer.Domain.Interfaces;
 using GoodHamburguer.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace GoodHamburguer.Infrastructure.Repositories;
 
-public class PedidoRepository
+public class PedidoRepository : IPedidoRepository
 {
     private readonly AppDbContext _context;
 
@@ -13,31 +14,31 @@ public class PedidoRepository
         _context = context;
     }
 
-    public async Task Adicionar(Pedido pedido)
+    public async Task AdicionarAsync(Pedido pedido)
     {
         await _context.Pedidos.AddAsync(pedido);
         await _context.SaveChangesAsync();
     }
 
-    public async Task<List<Pedido>> ObterTodos()
+    public async Task<List<Pedido>> ObterTodosAsync()
     {
         return await _context.Pedidos.ToListAsync();
     }
 
-    public async Task<Pedido?> ObterPorId(Guid id)
+    public async Task<Pedido?> ObterPorIdAsync(Guid id)
     {
         return await _context.Pedidos.FirstOrDefaultAsync(x => x.Id == id);
     }
 
-    public async Task Remover(Pedido pedido)
+    public async Task AtualizarAsync(Pedido pedido)
     {
-        _context.Pedidos.Remove(pedido);
+        _context.Pedidos.Update(pedido);
         await _context.SaveChangesAsync();
     }
 
-    public async Task Atualizar(Pedido pedido)
+    public async Task RemoverAsync(Pedido pedido)
     {
-        _context.Pedidos.Update(pedido);
+        _context.Pedidos.Remove(pedido);
         await _context.SaveChangesAsync();
     }
 }
