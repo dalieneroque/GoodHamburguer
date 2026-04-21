fc5d1f711c01dcc55faa4b2a45930e147c4c2396
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-20 22:55:07 -0300

    feat(dominio): adiciona extension method ObterPreco em Bebida e Acompanhamento

---
+++ GoodHamburguer.Domain/Extensions/AcompanhamentoExtensions.cs
@@ -0,0 +1,13 @@
+﻿using GoodHamburguer.Domain.Enums;
+
+namespace GoodHamburguer.Domain.Extensions
+{
+    public static class AcompanhamentoExtensions
+    {
+        public static decimal ObterPreco(this Acompanhamento acompanhamento) => acompanhamento switch
+        {
+            Acompanhamento.BatataFrita => 2.00m, 
+            _ => throw new ArgumentOutOfRangeException(nameof(acompanhamento))
+        };
+    }
+}
---
+++ GoodHamburguer.Domain/Extensions/BebidaExtensions.cs
@@ -0,0 +1,13 @@
+﻿using GoodHamburguer.Domain.Enums;
+
+namespace GoodHamburguer.Domain.Extensions
+{
+    public static class BebidaExtensions
+    {
+        public static decimal ObterPreco(this Bebida bebida) => bebida switch
+        {
+            Bebida.Refrigerante => 2.50m,
+            _ => throw new ArgumentOutOfRangeException(nameof(bebida))
+        };
+    }
+}
