a16af506358e74aab2ac37c73620c880b2ac5db5
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-20 22:07:33 -0300

    feat(Application): adiciona DTO CriarPedidoRequest

---
+++ GoodHamburguer.Application/DTOs/CriarPedidoRequest.cs
@@ -0,0 +1,9 @@
+﻿using GoodHamburguer.Domain.Enums;
+
+namespace GoodHamburguer.API.DTOs;
+
+public record CriarPedidoRequest(
+    Sanduiche? Sanduiche,
+    Acompanhamento? Acompanhamento,
+    Bebida? Bebida
+);
