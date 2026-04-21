78ef96a9fd2a91c6a7746911184aaef5f1eba3bc
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-20 21:26:14 -0300

    refactor(dominio): melhora Pedido com encapsulamento e construtor

--- GoodHamburguer.Domain/Entities/Pedido.cs
+++ GoodHamburguer.Domain/Entities/Pedido.cs
@@ -13,4 +13,29 @@ public class Pedido
     public decimal Subtotal { get; set; }
     public decimal Desconto { get; set; }
     public decimal Total { get; set; }
+
+    protected Pedido() { }
+
+    public Pedido(Sanduiche? sanduiche, Acompanhamento? acompanhamento, Bebida? bebida)
+    {
+        Sanduiche = sanduiche;
+        Acompanhamento = acompanhamento;
+        Bebida = bebida;
+    }
+
+    public void AtualizarItens(Sanduiche? sanduiche, Acompanhamento? acompanhamento, Bebida? bebida)
+    {
+        Sanduiche = sanduiche;
+        Acompanhamento = acompanhamento;
+        Bebida = bebida;
+    }
+
+    public void AplicarCalculo(decimal subtotal, decimal desconto, decimal total)
+    {
+        Subtotal = subtotal;
+        Desconto = desconto;
+        Total = total;
+    }
 }
+
+
