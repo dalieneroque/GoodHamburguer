e40dc2bb149bffe5b4d1c490d128a9d5075a28c3
Daliene Nonato Lima Roque <dalienenl@gmail.com>
2026-04-20 21:49:20 -0300

    refactor(Infrastructure): adiciona mapeamento de colunas e conversao de enums no DbContext

--- GoodHamburguer.Infrastructure/Data/AppDbContext.cs
+++ GoodHamburguer.Infrastructure/Data/AppDbContext.cs
@@ -6,9 +6,35 @@ namespace GoodHamburguer.Infrastructure.Data;
 public class AppDbContext : DbContext
 {
     public DbSet<Pedido> Pedidos { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Pedido>(entidade =>
+        {
+            entidade.HasKey(p => p.Id);
+
+            entidade.Property(p => p.Subtotal)
+                   .HasColumnType("decimal(18,2)"); 
+
+            entidade.Property(p => p.Desconto)
+                    .HasColumnType("decimal(18,2)");
+
+            entidade.Property(p => p.Total)
+                    .HasColumnType("decimal(18,2)");
+
+            entidade.Property(p => p.Sanduiche)
+                    .HasConversion<string>();
+
+            entidade.Property(p => p.Acompanhamento)
+                    .HasConversion<string>();
+
+            entidade.Property(p => p.Bebida)
+                    .HasConversion<string>(); 
+
+
+        });
     }
+
 }
